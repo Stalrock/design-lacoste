a615ee65b5aa4faf1a9adf6a75bd1299fe34bfd2
benjamin rossin <benjamin.rossin@gmail.com>
2019-09-05 20:03:58 +0200

    Design Lacoste | END

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <!-- LINK -->
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/media.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/icon.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+        <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+        <!-- META -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Site e-commerce de Benjamin Rossin">
+        <meta name="author" content="ROSSIN Benjamin">
+        <!-- SCRIPT -->
+        <script src="assets/js/jquery.js"></script>
+        <title>Site E-Commerce-lowcoste</title>
+    </head>
+    <body>
+        <header role="banner" class="header">
+            <div class="container">
+                <nav class="navbar pad fcontent fjustify-between align-baseline">
+                    <div class="logo-menu fcontent align-center">
+                        <a href="#" title="Aller vers l'accueil">Lowcoste</a>
+                    </div>
+                    <ul class="content-menu fcontent fjustify-center">
+                        <li class="menu-item fcontent align-center">
+                            <a class="menu-link" href="#">Homme</a>
+                            <div class="sub-nav pad">
+                                <div class="container">
+                                    <div class="content-sub-nav fcontent fadeIn animated">
+                                        <ul class="sub-nav-menu">
+                                            <span>vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item fcontent align-center">
+                            <a class="menu-link" href="#">Femme</a>
+                            <div class="sub-nav pad">
+                                <div class="container">
+                                    <div class="content-sub-nav fcontent fadeIn animated">
+                                        <ul class="sub-nav-menu">
+                                            <span>Vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers & Sweatshirts</a></li>
+                                            <li><a href="#">Robes & jupes</a></li>
+                                            <li><a href="#">Pantalons & shorts</a></li>
+                                            <li><a href="#">Chemises & Hauts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Chaussures</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers & Sweatshirts</a></li>
+                                            <li><a href="#">Robes & jupes</a></li>
+                                            <li><a href="#">Pantalons & shorts</a></li>
+                                            <li><a href="#">Chemises & Hauts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Maroquinerie</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Accessoires</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item fcontent align-center">
+                            <a class="menu-link" href="#">Enfant</a>
+                            <div class="sub-nav pad">
+                                <div class="container">
+                                    <div class="content-sub-nav fcontent fadeIn animated fast">
+                                        <ul class="sub-nav-menu">
+                                            <span>Vêtements</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers & Sweatshirts</a></li>
+                                            <li><a href="#">Robes & jupes</a></li>
+                                            <li><a href="#">Pantalons & shorts</a></li>
+                                            <li><a href="#">Chemises & Hauts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Chaussures</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers & Sweatshirts</a></li>
+                                            <li><a href="#">Robes & jupes</a></li>
+                                            <li><a href="#">Pantalons & shorts</a></li>
+                                            <li><a href="#">Chemises & Hauts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Maroquinerie</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                        <ul class="sub-nav-menu">
+                                            <span>Accessoires</span>
+                                            <li><a href="#">Polos</a></li>
+                                            <li><a href="#">Manteaux & Blousons</a></li>
+                                            <li><a href="#">Pullovers</a></li>
+                                            <li><a href="#">Sweatshirts</a></li>
+                                            <li><a href="#">T-Shirts</a></li>
+                                            <li><a href="#">Chemises</a></li>
+                                            <li><a href="#">Pantalons & Bermudas</a></li>
+                                            <li><a href="#">Vêtements de sport</a></li>
+                                            <li><a href="#">Maillot de bain</a></li>
+                                            <li><a href="#">Sous-vêtements</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="menu-item fcontent align-center">
+                            <a class="menu-link" href="#">Personnaliser</a>
+                        </li>
+                        <li class="menu-item fcontent align-center">
+                            <a class="menu-link" href="#">Qui est Lacoste ?</a>
+                        </li>
+                    </ul>
+                    <ul class="content-menu-icons fcontent">
+                        <li><a href="#" title="Mon panier"><img class="icon-basket" src="assets/img/basket.png"/></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <div role="main" class="main-container">
+            <div class="title-page">
+                <span>Chemises Homme (6)</span>
+            </div>
+            <div class="container">
+                <div class="container-products fcontent fwrap">
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img1.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise regular fit à capuche en flanelle de coton à carreaux</a></span>
+                            <span class="price-product">43,50 €</span>
+                            <span class="couleur-product">+ 1 couleur</span>
+                        </div>
+                    </div>
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img2.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise slim fit en pinpoint de coton stretch uni</a></span>
+                            <span class="price-product">67,50 €</span>
+                        </div>
+                    </div>
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img3.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise slim fit en popeline jacquard à pois</a></span>
+                            <span class="price-product">110,00 €</span>
+                            <span class="couleur-product">+ 3 couleurs</span>
+                        </div>
+                    </div>
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img3.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise slim fit en popeline jacquard à pois</a></span>
+                            <span class="price-product">110,00 €</span>
+                            <span class="couleur-product">+ 3 couleurs</span>
+                        </div>
+                    </div>
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img1.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise regular fit à capuche en flanelle de coton à carreaux</a></span>
+                            <span class="price-product">43,50 €</span>
+                            <span class="couleur-product">+ 1 couleur</span>
+                        </div>
+                    </div>
+                    <div class="product pad">
+                        <div class="img-product">
+                            <a href="#">
+                                <img src="assets/img/img2.jpg" alt="Une photo d'un produit"/>
+                            </a>
+                        </div>
+                        <div class="content-product fcontent fcolumn">
+                            <span  class="name-product"><a href="#">Chemise slim fit en pinpoint de coton stretch uni</a></span>
+                            <span class="price-product">67,50 €</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
